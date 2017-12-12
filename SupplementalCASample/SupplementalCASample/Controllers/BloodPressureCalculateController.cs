16f7b88e0889c60f8c5227786e41e2ae783cb9b5
Kevin lambe <kevin.lambe1@ie.ibm.com>
2017-12-12 21:45:57 +0000

    added small project with custom validation

---
+++ SupplementalCASample/SupplementalCASample/Controllers/BloodPressureCalculateController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SupplementalCASample.Models;
+
+namespace SupplementalCASample.Controllers
+{
+    public class BloodPressureCalculateController : Controller
+    {
+        // GET: BloodPressureCalculate
+        [HttpGet]
+        public ActionResult Calculate()
+        {
+            BloodPressure b1 = new BloodPressure();
+            b1.DiastolicPressure = 60;
+            b1.SystolicPressure = 100;
+            b1.UserBloodPressure = b1.CalculateBloodPressurecategory();
+
+            return View(b1);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(BloodPressure b1)
+        {
+
+            b1.UserBloodPressure = b1.CalculateBloodPressurecategory();
+            return View(b1);
+        }
+    }
+}
---
+++ SupplementalCASample/SupplementalCASample/Models/BloodPressure.cs
@@ -0,0 +1,47 @@
+﻿using Foolproof;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace SupplementalCASample.Models
+{
+    public enum BloodPressureCategory { low,Ideal, Prehigh, High}
+    public class BloodPressure
+    {
+        [Range(70, 140)]
+        public int SystolicPressure { get; set; }
+
+        [Range(40, 100)]
+        [LessThan("SystolicPressure")]
+        public int DiastolicPressure { get; set; }
+
+        [Display(Name = "Your Blood Pressure is: ")]
+        public BloodPressureCategory UserBloodPressure { get; set; }
+
+        public BloodPressureCategory CalculateBloodPressurecategory()
+        {
+            if(SystolicPressure<= 90)
+            {
+               return BloodPressureCategory.low;
+            }
+            else if (SystolicPressure <= 120)
+            {
+                return BloodPressureCategory.Ideal;
+            }
+            else if (SystolicPressure <= 140)
+            {
+                return BloodPressureCategory.Prehigh;
+            }
+            else 
+            {
+                return BloodPressureCategory.High;
+            }
+        }
+
+         
+
+    }
+}
